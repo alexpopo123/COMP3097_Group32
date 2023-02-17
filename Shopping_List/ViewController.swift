8b88fd1f7daeb502256596d6c6928a743f3d62a5
alex tsang <alextsangjoker888@gmail.com>
2023-02-17 16:37:05 -0500

    first commit

---
+++ Shopping_List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping_List
+//
+//  Created by 曾童波 on 2023-02-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
